687e36c2c48a08edbd023d4329604d6364eef294
Sujal Kumar Sinha <sujalksinha23@gmail.com>
2023-12-01 01:36:46 +0530

    add hover effect on images

---
+++ Landing Page/script.js
@@ -0,0 +1,26 @@
+gsap.from("#image1", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 1,
+  y: 60,
+});
+
+gsap.from("#image2", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 1,
+  x: 60,
+});
+
+gsap.from("#image3", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 1,
+  y: -60,
+});
+
+gsap.from(".main h1", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 1,
+});
